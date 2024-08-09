b5a0f73300a112e58b592d59dd362816b9a4b0d3
Zhanibek <zhanibek.om@gmail.com>
2024-08-09 17:12:41 +0900

    rm css

--- serve.jl
+++ serve.jl
@@ -23,7 +23,6 @@ function export_to_html(filename)
         <meta charset="utf-8">
         <title>Live-Server Test</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <!-- <link rel="stylesheet" href="/css/master.css"> -->
       </head>
       <body>
         <div class="container">
