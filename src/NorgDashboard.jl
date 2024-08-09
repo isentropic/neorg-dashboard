2ffdc39c5eb488e496fd1179f8d1546981bf255e
Zhanibek <zhanibek.om@gmail.com>
2024-08-09 18:31:12 +0900

    better styling

--- src/NorgDashboard.jl
+++ src/NorgDashboard.jl
@@ -19,6 +19,41 @@ function export_to_html(filename, temp_dir)
         <meta charset="utf-8">
         <title>Live-Server Test</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+        <style>
+        h1 {
+            font-size: 1em;
+            color: #007bff;
+            background-color: rgba(0, 123, 255, 0.1);
+            padding: 0.5em;
+            font-weight: bold;
+            margin-top: 1em;
+            margin-bottom: 1em;
+            border-left: 5px solid #007bff;
+            padding-left: 10px;
+        }
+
+        h2 {
+            font-size: 1.4em;
+            background-color: rgba(108, 117, 125, 0.1);
+            padding: 0.5em;
+            font-weight: semi-bold;
+            margin-top: 0.3em;
+            border-left: 5px solid #6c757d;
+            padding-left: 10px;
+        }
+
+        h3 {
+            font-size: 1.2em;
+            padding: 0.5em;
+            font-weight: normal;
+            margin-top: 0.5em;
+            border-left: 5px solid #28a745;
+            padding-left: 10px;
+        }
+
+        /* Additional heading levels as needed */
+        </style>
       </head>
       <body>
         <div class="container">
